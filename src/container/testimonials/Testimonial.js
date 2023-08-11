d9b9377359e2c1663ca0c18f777cd756b53302d3
ernkeyz <exconzy26@gmail.com>
2023-08-11 01:29:47 +0000

    v1.25 Test

--- src/container/testimonials/Testimonial.js
+++ src/container/testimonials/Testimonial.js
@@ -1,15 +1,101 @@
-import React from 'react'
+import React from 'react';
+import './Testimonials.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
 
-import './Testimonials.css'
+import ts1 from '../../assets/t1.png';
+import ts2 from '../../assets/t2.png';
+import ts3 from '../../assets/t3.png';
+import ts4 from '../../assets/t4.png';
 
 const Testimonial = () => {
   return (
     <div className="test-section">
-        <div className='test-top'>
-            <h2>This week specials!</h2>
+      <div className='test-top'>
+        <h2>What people say about us!</h2>
+      </div>
+      
+
+      <div className='hold-test'>
+      <div className='w-container'>
+      <div className='test-card'>
+        <div className='test-card-img'>
+            <img src={ts4} alt='ts4'/>
         </div>
-    </div>
-  )
+        <h4>Rexford Gates</h4>
+        <span>
+          {[1, 2, 3, 4, 5].map((ratingPoint, index) => (
+            <FontAwesomeIcon
+              key={index}
+              icon={faStar}
+              size="xs"
+            />
+          ))}
+        </span>
+        <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."</p>
+      </div>
+      </div>
+
+      <div className='w-container'>
+      <div className='test-card'>
+        <div className='test-card-img'>
+            <img src={ts1} alt='ts1'/>
+        </div>
+        <h4>Rexford Gates</h4>
+        <span>
+          {[1, 2, 3, 4, 5].map((ratingPoint, index) => (
+            <FontAwesomeIcon
+              key={index}
+              icon={faStar}
+              size="xs"
+            />
+          ))}
+        </span>
+        <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."</p>
+      </div>
+      </div>
+
+      <div className='w-container'>
+      <div className='test-card'>
+        <div className='test-card-img'>
+            <img src={ts2} alt='ts2'/>
+        </div>
+        <h4>Rexford Gates</h4>
+        <span>
+          {[1, 2, 3, 4, 5].map((ratingPoint, index) => (
+            <FontAwesomeIcon
+              key={index}
+              icon={faStar}
+              size="xs"
+            />
+          ))}
+        </span>
+        <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."</p>
+      </div>
+      </div>
+
+    <div className='w-container'>
+      <div className='test-card'>
+        <div className='test-card-img'>
+            <img src={ts3} alt='ts3'/>
+        </div>
+        <h4>Rexford Gates</h4>
+        <span>
+          {[1, 2, 3, 4, 5].map((ratingPoint, index) => (
+            <FontAwesomeIcon
+              key={index}
+              icon={faStar}
+              size="xs"
+            />
+          ))}
+        </span>
+        <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."</p>
+      </div>
+      </div>
+      </div>
+
+      </div>
+  );
 }
 
-export default Testimonial
+export default Testimonial;
