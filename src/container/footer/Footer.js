4b96a2eb1e2370fcb8b1f2dce8da8fbb464ac75b
ernkeyz <exconzy26@gmail.com>
2023-08-12 21:31:00 +0000

    v2.03

--- src/container/footer/Footer.js
+++ src/container/footer/Footer.js
@@ -1,8 +1,24 @@
 import React from 'react'
 
+import logo from '../../assets/logo-white.png';
+import './Footer.css';
+
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className="footer-section">
+      <div className='ft-container'>
+        <img src={logo} alt='logo'/>
+        <div className='site-map'>
+        <h3>sitemap</h3>
+        <p><a href='#home'>Home</a></p>
+        <p><a href='#home'>About</a></p>
+        <p><a href='#home'>Menu</a></p>
+        <p><a href='#home'>Reservations</a></p>
+        <p><a href='#home'>Order Online</a></p>
+        <p><a href='#home'>Login</a></p>
+        </div>
+      </div>
+    </div>
   )
 }
 
