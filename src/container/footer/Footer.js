2bb18636fe27b0e71cd85d9804d3617f34fb31bb
ernkeyz <exconzy26@gmail.com>
2023-08-13 18:27:57 +0000

    v2.04

--- src/container/footer/Footer.js
+++ src/container/footer/Footer.js
@@ -4,12 +4,14 @@ import logo from '../../assets/logo-white.png';
 import './Footer.css';
 
 const Footer = () => {
+  const textTopass = "Sitemap";
+
   return (
     <div className="footer-section">
       <div className='ft-container'>
         <img src={logo} alt='logo'/>
         <div className='site-map'>
-        <h3>sitemap</h3>
+        <h3>{textTopass}</h3>
         <p><a href='#home'>Home</a></p>
         <p><a href='#home'>About</a></p>
         <p><a href='#home'>Menu</a></p>
