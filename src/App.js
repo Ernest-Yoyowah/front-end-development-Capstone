6024ef86440ebabcd6d1e7e44222ad58c2abf223
Ernest-Yoyowah <117729383+Ernest-Yoyowah@users.noreply.github.com>
2023-08-13 17:49:20 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import {Routes, Route} from 'react-router-dom';
-
 import { Navbar} from './components';
 import { Header, Main, Footer, Testimonial, About } from './container';
 
