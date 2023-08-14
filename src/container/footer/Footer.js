3e06ba9796769fca38e39ea502fb1ab8d9eded51
ernkeyz <exconzy26@gmail.com>
2023-08-14 21:00:06 +0000

    footer section final:front end

--- src/container/footer/Footer.js
+++ src/container/footer/Footer.js
@@ -3,6 +3,10 @@ import React from 'react'
 import logo from '../../assets/logo-white.png';
 import './Footer.css';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMapMarkerAlt, faPhone, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faFacebook, faInstagram, faTwitter, faYoutube } from '@fortawesome/free-brands-svg-icons';
+
 const Footer = () => {
   const textTopass = "Sitemap";
   const conHead = "Constact Us";
@@ -22,6 +26,25 @@ const Footer = () => {
         </div>
         <div className='contact-section'>
           <h3>{conHead}</h3>
+          <p><FontAwesomeIcon icon={faMapMarkerAlt} className="white-icon" /> 678 Pisa Ave, Chicago, IL 60611</p>
+          <p><FontAwesomeIcon icon={faPhone} className="white-icon" /> (312) 593-2744</p>
+          <p><FontAwesomeIcon icon={faEnvelope} className="white-icon" /> customer@littlelemon.com</p>
+        </div>
+
+        <div className='connect'>
+          <h3>Connect with us</h3>
+          <a href='#'>
+            <FontAwesomeIcon icon={faFacebook} className="white-icon" />
+          </a>
+          <a href='#'>
+            <FontAwesomeIcon icon={faInstagram} className="white-icon" />
+          </a>
+          <a href='#'>
+            <FontAwesomeIcon icon={faTwitter} className="white-icon" />
+          </a>
+          <a href='#'>
+            <FontAwesomeIcon icon={faYoutube} className="white-icon" />
+          </a>
         </div>
       </div>
     </div>
