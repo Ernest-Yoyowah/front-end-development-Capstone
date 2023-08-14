801bb3aa618415eb40a38dbcf84d84a3938f524d
ernkeyz <exconzy26@gmail.com>
2023-08-14 19:53:03 +0000

    footer-section update 2.01

--- src/container/footer/Footer.js
+++ src/container/footer/Footer.js
@@ -5,19 +5,23 @@ import './Footer.css';
 
 const Footer = () => {
   const textTopass = "Sitemap";
+  const conHead = "Constact Us";
 
   return (
     <div className="footer-section">
       <div className='ft-container'>
         <img src={logo} alt='logo'/>
         <div className='site-map'>
-        <h3>{textTopass}</h3>
-        <p><a href='#home'>Home</a></p>
-        <p><a href='#home'>About</a></p>
-        <p><a href='#home'>Menu</a></p>
-        <p><a href='#home'>Reservations</a></p>
-        <p><a href='#home'>Order Online</a></p>
-        <p><a href='#home'>Login</a></p>
+          <h3>{textTopass}</h3>
+            <p><a href='#home'>Home</a></p>
+            <p><a href='#home'>About</a></p>
+            <p><a href='#home'>Menu</a></p>
+            <p><a href='#home'>Reservations</a></p>
+            <p><a href='#home'>Order Online</a></p>
+            <p><a href='#home'>Login</a></p>
+        </div>
+        <div className='contact-section'>
+          <h3>{conHead}</h3>
         </div>
       </div>
     </div>
